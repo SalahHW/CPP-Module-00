0aa5ecce20e0a71dd0178dca2ee52e116f2f7448
Salah <salah.bouheniche@gmail.com>
2023-11-10 02:31:07 +0100

    Add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 02:08:38 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/11/10 02:11:41 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+}
