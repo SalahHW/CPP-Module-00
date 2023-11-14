fe8a73883505412d12feb9db78482d8851e53a16
Salah <salah.bouheniche@gmail.com>
2023-11-14 22:42:18 +0100

    Change newline in megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 02:08:38 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/11/10 17:11:25 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/11/11 05:45:12 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static void	print_in_upper_case(char **str)
 			j++;
 		}
 		if (str[i + 1])
-			std::cout << " ";
+			std::cout << std::endl;
 	}
 }
 
@@ -35,5 +35,5 @@ int	main(int argc, char **argv)
 	if (argc > 1)
 		print_in_upper_case(argv);
 	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
